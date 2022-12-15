d3ad6c1de0f25ed133e26e668065b78cf063cf9d
Erik Harutyunyan <82722153+ErikHarutyunyan@users.noreply.github.com>
2022-12-15 10:17:21 +0400

    Update letcode203.js

--- letcode203.js
+++ letcode203.js
@@ -1,3 +1,20 @@
+/*
+ Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ Example 1:
+ Input: head = [1,2,6,3,4,5,6], val = 6
+ Output: [1,2,3,4,5]
+
+ Example 2:
+ Input: head = [], val = 1
+ Output: []
+
+ Constraints:
+ The number of nodes in the list is in the range [0, 104].
+ 1 <= Node.val <= 50
+ 0 <= val <= 50
+*/
+
 const removeElements = (head, val) => {
   if (!head) return null;
   let cur = head.next;
