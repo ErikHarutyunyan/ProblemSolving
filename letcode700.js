43c2bbd5d8fd7714566a313990caab0d3c44b8c2
Erik Harutyunyan <82722153+ErikHarutyunyan@users.noreply.github.com>
2022-12-16 22:48:40 -0800

    16.12.22

---
+++ letcode700.js
@@ -0,0 +1,25 @@
+/**
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+Example 1:
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+ */
+
+const searchBST = function (root, val) {
+  if (root === null) return null;
+  if (val === undefined) return root;
+  let cur = root;
+  while (cur) {
+    if (cur.val == val) return cur;
+    else if (val < cur.val) cur = cur.left;
+    else cur = cur.right;
+  }
+  return null;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
